b6c19d155b9deeb82d793d32d1fd702e96440570
Luz Cavallin <luzcavallin@gmail.com>
2022-12-12 13:34:46 -0300

    se

--- app.js
+++ app.js
@@ -1,53 +1,109 @@
-let cantidadVape;
+// venta de vapes, mayor a 18 años, eleccion de sabor y puffs.
+
+/*
+let edad;
 let sabor;
 let puffs;
+let respuesta;
 
-cantidadVape = parseInt (prompt("Hola! Ingrese la cantidad de vapes"));
+edad = parseInt (prompt("Hola! Ingrese su edad"));
 
-while (cantidadVape <=1) {
+while (edad >=18) {
     alert ("Puede continuar su compra")
-    break;
 }
-
-sabor = prompt (`Que sabor desea comprar?:
-1: grape
-2: peach
-3: strawberry
-`);
-
-puffs = prompt (`Cuantos puffs desea?:
-1: 1500puffs
-2: 2500puufs
-3: 3500puffs`)
 
 function elegirSabor () {
+    sabor = prompt (`Que sabor desea comprar?:
+    1: grape
+    2: peach
+    3: strawberry
+    `);
+    let respuesta ="";
     switch (sabor) {
         case "1":
-            alert ("Elegiste sabor grape");
+            respuesta = "grape";
             break;
         case "2":
-            alert ("Elegiste sabor peach");
+            respuesta = "peach";
             break;
         case "3":
-            alert ("Elegiste sabor strawberry");
+            respuesta = "strawberry";
             break;
     } 
+    return respuesta;
 }
+
+sabor = elegirSabor ();
+alert (`el sabor elegido es` + " " + sabor)
 
 function elegirPuffs () {
+    puffs = prompt (`Cuantos puffs desea?:
+    1: 1500puffs
+    2: 2500puufs
+    3: 3500puffs`);
+    let respuesta = "";
     switch (puffs) {
         case "1":
-            alert ("Elegiste 1500puffs");
+            respuesta = "1500 puffs";
             break;
         case "2":
-            alert ("Elegiste 2500puffs");
+            respuesta = "2500 puffs";
             break;
         case "3":
-                alert ("Elegiste 3500puffs");
+            respuesta = "2500 puffs";
                 break;
     }
+    return respuesta;
 }
 
-elegirSabor ();
-elegirPuffs (); 
+puffs = elegirPuffs ();
+alert (`elegiste` + " " + puffs)
+
+*/
+
+
+// SEGUNDA PRE-ENTREGA
+
+let edad;
+edad = parseInt (prompt("Hola! Ingrese su edad"));
+
+if (edad >= 18){
+    alert ("Puede continuar con la compra")
+} else { 
+    alert ("No puede continuar con la compra")
+    
+}
+
+const productos = [
+    {nombre: "vape 800puffs", precio: 4300 },
+    {nombre: "vape 1500puffs", precio: 5200 },
+    {nombre: "vape 2500puffs" , precio : 6500},
+    {nombre: "vape 3500puffs" , precio: 7500},
+    {nombre: "equipo recargable" , precio: 9500},
+    
+]
+
+productos.forEach((producto) => {
+    console.log (producto.nombre);
+    console.log (producto.precio);
+})
+
+function filtrarVapes (productos) {
+    const vapes = productos.filter((productos) => productos.nombre.includes ("vape")) ;
+    console.log(vapes)
+    }
+
+function filtrarVapes (productos) {
+    const vapes = productos.filter((productos) => productos.nombre.includes ("recargable")) ;
+    console.log(vapes)
+    }
+
+filtrarVapes(productos);
+
+// SUMA TOTAL DE LA COMPRA//
+
+const totalCarrito = productos.reduce((acumulador,producto) => acumulador + producto.precio, 0)
+console.log(totalCarrito);
+
+
 
